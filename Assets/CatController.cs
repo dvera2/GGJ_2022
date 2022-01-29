5c5bbf9d4ca0956c00ff6e5a1bac52e0e0897182
Karen Haren <photo.karen@gmail.com>
2022-01-28 23:19:03 -0800

    started movement.

--- Assets/CatController.cs
+++ Assets/CatController.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    Rigidbody body;
+
+    [SerializeField]
+    float jumpHeight;
+
+    bool isJumpingPressed;
+    bool isJumping;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +23,45 @@ public class CatController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float direction = Input.GetAxis("Horizontal");
+ 
         
+        if (!isJumping)
+        {
+            body.velocity = new Vector3(0, body.velocity.y, 0);
+            //Vector3 movement = (Vector3.right * Time.deltaTime * direction * speed);
+            Vector3 movement = (Vector3.right * direction * speed);
+            body.AddForce(movement, ForceMode.VelocityChange);
+        }
+        
+
+        if (!isJumping && isJumpingPressed)
+        {
+            body.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
+            isJumpingPressed = false;
+            isJumping = true;
+            Debug.Log("force is applied");
+        }
+    }
+
+    private void Update()
+    {
+        //if jumpinging isnt already pressed then read the input
+        if (!isJumpingPressed && !isJumping)
+        {
+            isJumpingPressed = Input.GetKey(KeyCode.Space);          
+            if (isJumpingPressed)
+            {
+                Debug.Log("jumped");
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision otherThing)
+    {
+        isJumping = false;
     }
 }
