97ad0ee33b2f5db7305fbcfe34c9c8672a6441f3
Karen Haren <photo.karen@gmail.com>
2022-01-29 12:55:21 -0800

    movement and animations weird, but working.

--- Assets/CatController.cs
+++ Assets/CatController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
+    [Header("Movement")]
+    public Animator CatAnim;
+    public GameObject CatSprites;
+    
+
+    [Header("Movement")]
     [SerializeField]
     float speed;
 
@@ -16,6 +22,7 @@ public class CatController : MonoBehaviour
     bool isJumpingPressed;
     bool isJumping;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,21 +34,27 @@ public class CatController : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
  
-        
+ //Walk       
         if (!isJumping)
         {
             body.velocity = new Vector3(0, body.velocity.y, 0);
             //Vector3 movement = (Vector3.right * Time.deltaTime * direction * speed);
             Vector3 movement = (Vector3.right * direction * speed);
             body.AddForce(movement, ForceMode.VelocityChange);
-        }
-        
+            SetDirection(direction);
 
+        }
+
+
+
+        
+//Jump
         if (!isJumping && isJumpingPressed)
         {
             body.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
             isJumpingPressed = false;
             isJumping = true;
+
             Debug.Log("force is applied");
         }
     }
@@ -54,6 +67,7 @@ public class CatController : MonoBehaviour
             isJumpingPressed = Input.GetKey(KeyCode.Space);          
             if (isJumpingPressed)
             {
+                CatAnim.SetBool("isJumping", true);
                 Debug.Log("jumped");
             }
             
@@ -62,6 +76,41 @@ public class CatController : MonoBehaviour
 
     private void OnCollisionEnter(Collision otherThing)
     {
-        isJumping = false;
+        if (otherThing.gameObject.tag == "floor")
+        {
+            isJumping = false;
+            CatAnim.SetBool("isJumping", false);
+        }
+        
+    }
+
+    void SetDirection(float dir)
+    {
+        if(dir == 0)
+        {
+            return;
+        }
+        Vector3 CatScale = CatSprites.transform.localScale;
+        CatScale.x = Mathf.Abs(CatScale.x) * Mathf.Sign(dir);
+        CatSprites.transform.localScale = CatScale;
+        CatAnim.SetFloat("Walking", Mathf.Abs(dir));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hiding");
+        if (other.tag == "Hide")
+        {
+            CatAnim.SetBool("isHiding", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Out");
+        if (other.tag == "Hide")
+        {
+            CatAnim.SetBool("isHiding", false);
+        }
     }
 }
