1424ddc93d805953d54c492112f4a8a7aa98bd16
Karen Haren <photo.karen@gmail.com>
2022-01-29 16:13:04 -0800

    mostly working in Karen scene. There is a timer now. Working on a timer for when the human is around.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,10 +8,20 @@ public class GameManager : MonoBehaviour
 
     public GameObject Cat;
     public GameObject SafeMessage;
+    public GameObject Human;
+    public GameObject humanIcon;
 
     public TextMeshProUGUI score;
+    public TextMeshProUGUI Safetimer;
+    public TextMeshProUGUI caught;
+
+    float safeTime;
+    bool safeTimerIsRunning;
+
+    float unsafeTime;
 
     public bool isHidden;
+    public bool isHumanAround;
 
     int points;
 
@@ -20,11 +30,45 @@ public class GameManager : MonoBehaviour
     {
         points = 0;
         score.text = "Points:"+ points;
+        safeTimerIsRunning = true;
+
+        setSafeTimer();
+        
+        Human.SetActive(false);
+        humanIcon.SetActive(false);
+        caught.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (safeTimerIsRunning)
+        {
+            if (safeTime > 0)
+            {
+                safeTime -= Time.deltaTime;
+                Safetimer.text = Mathf.Floor(safeTime).ToString();
+            }
+        }
+
+        if (safeTime <= 0)
+        {
+            safeTime = 0;
+            Safetimer.text = Mathf.Floor(safeTime).ToString();
+            makeHumanActive();
+            safeTimerIsRunning = false;
+        }
+
+
+        if (isHumanAround)
+        {
+            makeHumanActive();
+
+            if (!isHidden)
+            {
+                yourCaught();
+            }
+        }
         
     }
 
@@ -42,7 +86,32 @@ public class GameManager : MonoBehaviour
 
     public void AddPoint()
     {
+        if (!safeTimerIsRunning)
+        {
+            yourCaught();
+        }
         points++;
         score.text = "Points:" + points;
     }
+
+    void makeHumanActive()
+    {
+        Human.SetActive(true);
+        humanIcon.SetActive(true);
+        
+    }
+
+    void yourCaught()
+    {
+        caught.gameObject.SetActive(true);
+    }
+
+    void setSafeTimer()
+    {
+        float randomTime = Random.Range(2,5);
+        safeTime = randomTime;
+        Safetimer.text = safeTime.ToString();
+        Debug.Log("Timer is set to "+safeTime);
+    }
+
 }
